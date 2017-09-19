7a4f40024abd9d5ce3f78ad1fefd39b2422ee15a
Mohan Kumar <jimbono@gmail.com>
2017-09-19 00:38:08 -0500

    first commit

---
+++ visa.poc.api.legacy/VisaApiClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace visa.poc.api
+{
+    public class VisaApiClient
+    {        
+        X509Certificate2 _clientCertificate;
+        string _basicAuthHeader;        
+
+        public VisaApiClient(string baseApiUrl, string certificatePath, string certificatePassword, string apiUserId, string apiPassword)
+        {            
+            _clientCertificate = new X509Certificate2(certificatePath, certificatePassword);
+            _basicAuthHeader = getBasicAuthHeader(apiUserId, apiPassword);
+            
+
+
+        }
+
+        public AddTravelItineraryResponse AddTravelItinerary (AddTravelItineraryRequest request)
+        {
+
+            //HttpWebRequest webRequest = WebRequest.Create(baseApiUrl) as HttpWebRequest;
+
+            //var response = await _visaApi.AddTravelItinerary(request, _basicAuthHeader);
+            return null;
+        }
+
+        private string getBasicAuthHeader(string userId, string password)
+        {
+            string authString = userId + ":" + password;
+            var authStringBytes = Encoding.UTF8.GetBytes(authString);
+            string authHeaderString = Convert.ToBase64String(authStringBytes);
+            return "Basic " + authHeaderString;
+        }
+
+        private void LogRequest(string url, string requestBody)
+        {
+            Debug.WriteLine(url);
+            Debug.WriteLine(requestBody);
+        }
+
+        private void LogResponse(string info, HttpWebResponse response)
+        {
+            string responseBody;
+            Debug.WriteLine(info);
+            Debug.WriteLine("Response Status: \n" + response.StatusCode);
+            Debug.WriteLine("Response Headers: \n" + response.Headers.ToString());
+
+            using (var reader = new StreamReader(response.GetResponseStream(), ASCIIEncoding.ASCII))
+            {
+                responseBody = reader.ReadToEnd();
+            }
+
+            Debug.WriteLine("Response Body: \n" + responseBody);
+        }
+    }
+}
---
+++ visa.poc.api.tests/VisaTravelNotificationServiceTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace visa.poc.api.tests
+{
+    [TestClass]
+    public class VisaTravelNotificationServiceTest
+    {
+        private VisaApiClient _visaApiClient;
+
+        public VisaTravelNotificationServiceTest()
+        {
+            var visaApiUrl = ConfigurationSettings.AppSettings["visaUrl"];
+            var certPath = ConfigurationSettings.AppSettings["cert"];
+            var certPassword = ConfigurationSettings.AppSettings["certPassword"];
+            var apiUserId = ConfigurationSettings.AppSettings["userId"];
+            var apiPassword = ConfigurationSettings.AppSettings["password"];
+
+            _visaApiClient = new VisaApiClient(visaApiUrl, certPath, certPassword, apiUserId, apiPassword);
+        }
+
+        [TestMethod]
+        public async Task AddItineraryTest()
+        {
+
+            var request = createItineraryRequest();
+            var response = await _visaApiClient.AddTravelItinerary(request);
+            Assert.AreEqual(200, response.responseCode);
+        }
+
+
+        private AddTravelItineraryRequest createItineraryRequest()
+        {
+            var addTravelItineraryRequest = new AddTravelItineraryRequest();
+
+            var addTravelItinerary = new AddTravelItinerary();
+
+            addTravelItinerary.departureDate = new DateTime(2017, 9, 18).ToString("yyyy-MM-dd");
+            addTravelItinerary.returnDate = new DateTime(2017, 9, 28).ToString("yyyy-MM-dd");
+
+            var destinations = new System.Collections.Generic.List<Destination>();
+            var destination = new Destination();
+            destination.country = "840";
+            destination.state = "CA";
+            destinations.Add(destination);
+            addTravelItinerary.destinations = destinations;
+
+
+            var primaryAccountNumbers = new System.Collections.Generic.List<PrimaryAccountNumber>();
+            var primaryAccountNumber = new PrimaryAccountNumber();
+            primaryAccountNumber.cardAccountNumber = "4645191800301234";
+
+            primaryAccountNumbers.Add(primaryAccountNumber);
+
+            addTravelItinerary.primaryAccountNumbers = primaryAccountNumbers;
+
+            addTravelItinerary.partnerBid = "12345678";
+            addTravelItinerary.userId = "MohanKumar";
+
+            addTravelItineraryRequest.addTravelItinerary = addTravelItinerary;
+
+            return addTravelItineraryRequest;
+        }
+    }
+}
